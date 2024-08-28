4f62b7681e07227f5323c8aad3fc8c08015a67f3
LLuizXL <luiz.mwlo76@gmail.com>
2024-08-28 11:06:45 -0300

    Anotações sobre BOOTSTRAP + Informações para estudo

--- AplicacaoMVCLinq/Controllers/ClienteController.cs
+++ AplicacaoMVCLinq/Controllers/ClienteController.cs
@@ -13,7 +13,7 @@ namespace AplicacaoMVCLinq.Controllers
         {
             Cliente c1 = new Cliente(10987, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
             Cliente c2 = new Cliente(23456, "Rocky", "mayersrakim@example.com", "4321-5687", new DateOnly(1988, 10, 3));
-            Cliente c3 = new Cliente(34567, "Drake", "champagnepapi@example.com", "4162-9952", new DateOnly(2000, 6, 1));
+            Cliente c3 = new Cliente(34567, "Drake", "champagnepapi@example.com", "4162-9952", new DateOnly(1990, 7, 3));
             Cliente c4 = new Cliente(45678, "Kendrick", "kdotREAL@example.com", "4162-9952", new DateOnly(2004, 3, 8));
             Cliente c5 = new Cliente(54321, "J.G Quintel", "mordecaiOOH@example.com", "4162-9952", new DateOnly(2010, 7, 28));
 
